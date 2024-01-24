c3511549642b4b8a0ce49a92daf6318c8729be53
Kishor Bhagat <kishorebhagat663@gmail.com>
2024-01-24 09:42:59 +0530

    Basic server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+const port = 8000 || process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json("Working")
+})
+
+app.listen(port, () => {
+    console.log("Server running at port", port)
+})
